3c9058285b6191803c9931b59c2b76a6b53f6e5d
Ackerblad <johanackerblad@outlook.com>
2023-12-12 17:47:46 +0100

    Rename, cleanup

---
+++ Shapes/GameWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Shapes
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class GameWindow : Window
+    {
+        private DispatcherTimer gameLoopTimer;
+
+        private PlayerControl playerControl;
+
+        private RandomBall randomBall;
+
+        private Player player;
+
+        private FileManager fileManager;
+
+        private int gameLoopTicks = 0;
+
+        private int secondsPassed = 0;
+
+        public GameWindow(Player player)
+        {
+            InitializeComponent();
+            GameCanvas.Focus();
+
+            this.player = player;
+            fileManager = new FileManager();
+
+            Shape playerShape = player.GetPlayerShape();
+            GameCanvas.Children.Add(playerShape);
+            Canvas.SetLeft(playerShape, 56);
+            Canvas.SetTop(playerShape, 200);
+
+            playerControl = new PlayerControl(GameCanvas, playerShape);
+            this.KeyDown += OnKeyDown;
+            this.KeyUp += OnKeyUp;
+
+            gameLoopTimer = new DispatcherTimer();
+            gameLoopTimer.Tick += GameLoopTimer_Tick;
+            gameLoopTimer.Interval = TimeSpan.FromMilliseconds(20); 
+            gameLoopTimer.Start();
+
+            randomBall = new RandomBall(GameCanvas);
+        }
+
+        //Main loop of the game
+        //Time clock ticks 50 ticks * 20 ms = 1000 ms (1 second)
+        private void GameLoopTimer_Tick(object sender, EventArgs e)
+        {
+            playerControl.MovePlayer();
+
+            gameLoopTicks++;
+            if (gameLoopTicks >= 50) 
+            {
+                UpdateTime();
+                gameLoopTicks = 0;
+            }
+
+            randomBall.MoveRandomBall();
+
+            if (randomBall.CheckForCollision(playerControl.PlayerShape))
+            {
+                GameOver();
+                UpdateHighScore();
+            } 
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        //Update player movement based on pressed keys
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.A:
+                    playerControl.GoLeft = true; 
+                    break;
+                case Key.D: 
+                    playerControl.GoRight = true; 
+                    break;
+                case Key.W: 
+                    playerControl.GoUp = true; 
+                    break;
+                case Key.S: 
+                    playerControl.GoDown = true; 
+                    break;
+            }
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.A:
+                    playerControl.GoLeft = false; 
+                    break;
+                case Key.D: 
+                    playerControl.GoRight = false; 
+                    break;
+                case Key.W: 
+                    playerControl.GoUp = false; 
+                    break;
+                case Key.S: 
+                    playerControl.GoDown = false; 
+                    break;
+            }
+        }
+
+        //Uppdate the clock by adding 1 second
+        private void UpdateTime()
+        {
+            secondsPassed++;
+            timeLbl.Content = secondsPassed.ToString(); 
+        }
+
+        //High score is based on the time clock
+        private void UpdateHighScore()
+        {
+            if (secondsPassed > player.HighScore)
+            {
+                player.HighScore = secondsPassed;
+            }
+        }
+
+        //Save high score to CSV and stop all game timers
+        //Restart or close the game depending on players choice
+        private void GameOver()
+        {
+            UpdateHighScore();
+            fileManager.SaveToCsv(player.PlayerName, player.SelectedShape, player.HighScore);
+
+            randomBall.ballCreationTimer.Stop();
+            gameLoopTimer.Stop();
+
+            MessageBoxResult result = MessageBox.Show("Game Over! Play again?", "Game Over", MessageBoxButton.YesNo);
+            if (result == MessageBoxResult.Yes)
+            {
+                GameWindow newGameWindow = new GameWindow(player);
+                newGameWindow.Show();
+
+                this.Close();
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+    }
+}
---
+++ Shapes/NewGame.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+
+namespace Shapes
+{
+    /// <summary>
+    /// Interaction logic for NewGame.xaml
+    /// </summary>
+    public partial class NewGame : Window
+    {
+        private FileManager fileManager;
+      
+        public NewGame()
+        {
+            InitializeComponent();
+            fileManager = new FileManager();
+            LoadPlayerNames();
+            ShowMessageBoxWithDelay();
+        }
+
+        private async void ShowMessageBoxWithDelay()
+        {
+            await Task.Delay(500);
+            MessageBox.Show("To start: Choose a name, form, add and then save.");
+        }
+
+        //Validate inputs,create player and open the Game
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Your goal is to not be hit by the balls. Use W-A-S-D to move. The longer you last, the more xp you get.");
+
+            if (SelectShape.SelectedItem != null && nameListBox.SelectedItem != null) 
+            {
+                ComboBoxItem selectedItem = (ComboBoxItem)SelectShape.SelectedItem;
+
+                string playerName = nameListBox.SelectedItem.ToString();
+
+                Player existingPlayer = fileManager.GetPlayerByName(playerName);
+
+                if (existingPlayer != null)
+                {
+                    GameWindow gameWindow = new GameWindow(existingPlayer);
+                    gameWindow.Show();
+                    Hide();
+                }
+                else
+                {
+                    MessageBox.Show("Player with the selected name doesn't exist! You need to save first!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Enter your name and select a shape to begin.");
+            }
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string newName = NameTextBox.Text.Trim();
+            string selectedShape = GetSelectedShape();
+
+            if (string.IsNullOrEmpty(selectedShape))
+            {
+                MessageBox.Show("You need to select a shape!");
+                return;
+            }
+
+            if (!string.IsNullOrEmpty(newName) && !nameListBox.Items.Contains(newName))
+            {
+                nameListBox.Items.Add(newName);
+            }
+        }
+
+        private void RemoveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (nameListBox.SelectedItem != null)
+            {
+                string playerNameToRemove = nameListBox.SelectedItem.ToString();
+
+                nameListBox.Items.Remove(playerNameToRemove);
+
+                fileManager.RemovePlayerFromCsv(playerNameToRemove);
+            }
+            else
+            {
+                MessageBox.Show("Select a name to remove.");
+            }
+        }
+
+        private string GetSelectedShape()
+        {
+            if (SelectShape.SelectedItem is ComboBoxItem selectedItem && selectedItem.Content != null)
+            {
+                return selectedItem.Content.ToString();
+            }
+            return "";
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> playerNames = nameListBox.Items.OfType<string>().ToList();
+
+            foreach (string playerName in playerNames)
+            {
+                string selectedShape = GetSelectedShape();
+                int highScore = 0;
+                fileManager.SaveToCsv(playerName, selectedShape, highScore);
+            }
+            MessageBox.Show("Players saved!");
+        }
+
+        private void LoadPlayerNames()
+        {
+            nameListBox.Items.Clear();
+            List<Player> loadedPlayers = fileManager.LoadFromCsv();
+            foreach (Player player in loadedPlayers)
+            {
+                nameListBox.Items.Add(player.PlayerName);
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+            this.Hide();
+        }
+    }
+}
