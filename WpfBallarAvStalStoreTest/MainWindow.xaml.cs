c47684fe4a5fd2323a83b81e798e0fc2c7a0d052
Josef Andersson <JA3652023@outlook.com>
2023-12-12 16:16:27 +0100

    test

---
+++ WpfBallarAvStalStoreTest/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfBallarAvStalStoreTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Player> playerList = new List<Player>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            //OneAtATime();
+            Player player = new Player("Vilhelm", 1, 1, 1, 0, 10);
+            Player player2 = new Player("Josef", 1, 1, 1, 0, 10);
+            Player player3 = new Player("David", 1, 1, 1, 0, 10);
+            Player player4 = new Player("Johan", 1, 1, 4, 0, 10);
+
+            playerList.Add(player);
+            playerList.Add(player2);
+            playerList.Add(player3);
+            playerList.Add(player4);
+
+            foreach (Player p in playerList)
+            {
+                PlayersListbox.Items.Add(p.Name + ": " + p.Exp + " Exp");
+            }
+            
+        }
+        private void OneAtATime()
+        {
+            if (SpeedBostListbox.SelectedItem != null)
+            {
+                ShapeListbox.SelectedItem = null;
+                ColourListbox.SelectedItem = null;
+                
+            }
+            if (ShapeListbox.SelectedItem != null)
+            {
+                SpeedBostListbox.SelectedItem = null;
+                ColourListbox.SelectedItem = null;
+            }
+            if (ColourListbox.SelectedItem != null)
+            {
+                SpeedBostListbox.SelectedItem = null;
+                ShapeListbox.SelectedItem = null;
+            }
+        }
+
+        private void BuyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (PlayersListbox.SelectedItem != null)
+            {
+                int index = PlayersListbox.SelectedIndex;
+                Player player = playerList[index];
+
+                if (SpeedBostListbox.SelectedItem != null)
+                {
+                    int speedindex = SpeedBostListbox.SelectedIndex;
+                    
+                    if (speedindex == 0)
+                    {
+                        player.Speed *= 1.1;
+                    }
+                    if (speedindex == 1)
+                    {
+                        player.Speed *= 1.25;
+                    }
+                    if (speedindex == 2)
+                    {
+                        player.Speed *= 1.5;
+                    }
+                    if (speedindex == 3)
+                    {
+                        player.Speed *= 2;
+                    }
+                    player.ShowPlayerInfo();
+                }
+                if (ShapeListbox.SelectedItem != null)
+                {
+                    int shapeindex = ShapeListbox.SelectedIndex;
+
+                    if (shapeindex == 0)
+                    {
+
+                    }
+
+                    player.ShowPlayerInfo();
+                }
+                if (ColourListbox.SelectedItem != null)
+                {
+                    //player.Color = 
+                    MessageBox.Show(player.Color.ToString());
+                }
+                
+                
+            }
+        }
+    }
+}
---
+++ WpfBallarAvStalStoreTest/Player.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfBallarAvStalStoreTest
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Shape { get; set; }
+        public int Color { get; set; }
+        public double Speed { get; set; }
+        public double Highscore { get; set; }
+        public double Exp { get; set; }
+        public Player(string name, int shape, int color, double speed, double highscore, double exp)
+        {
+            Name = name;
+            Shape = shape;
+            Color = color;
+            Speed = speed;
+            Highscore = highscore;
+            Exp = exp;
+        }
+        public void ShowPlayerInfo()
+        {
+            string strShape;
+            if (Shape > 0)
+            {
+                strShape = "Rectangle";
+            }
+            else { strShape = "Ellipse"; }
+
+            string strColor = "";
+            if (Color == 0)
+            {
+                strColor = "Red";
+            }
+            if (Color == 1)
+            {
+                strColor = "Green";
+            }
+            if (Color == 2)
+            {
+                strColor = "Yellow";
+            }
+            if (Color == 3)
+            {
+                strColor = "Blue";
+            }
+            MessageBox.Show($"{Name} \nShape: {strShape}. \nColor: {strColor}. \nSpeed: {Speed}. \nHighscore: {Highscore}. \nExp: {Exp}.");
+        }
+    }
+}
